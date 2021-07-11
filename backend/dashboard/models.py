5e0c902e3a8f6a331c3066b7d64db8de27c82e43
moosavi‏ <@gmail.com>
2021-07-11 19:30:56 +0430

    Add message api

--- backend/dashboard/models.py
+++ backend/dashboard/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserData(models.Model):
+    user = models.ForeignKey(User, on_delete= models.DO_NOTHING)
+
+class Message(models.Model):
+    class Meta:
+        verbose_name='a message'
+        verbose_name_plural='All messages'
+    sender = models.ForeignKey(User, on_delete= models.DO_NOTHING, related_name="sender", verbose_name='Your name')
+    receiver = models.ForeignKey(User, on_delete= models.DO_NOTHING, related_name="receiver", verbose_name='Select your contact')
+    message = models.TextField('Your message')
--- backend/dashboard/views.py
+++ backend/dashboard/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from typing import cast
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Message
+from rest_framework import status
 
-# Create your views here.
+class MessageView(APIView):
+    def post(request, self):
+        message = Message(
+            sender = request.user,
+            receiver = int(request.data['receiver']),
+            message = request.data['message']
+        )
+        if message is not None:
+            message.save() 
+            return Response({"message": "saved"})
+        else:
+            return Response({"message":"something unexpected heppened"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
