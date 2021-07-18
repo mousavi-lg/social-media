1efbf24814d61a64f68c39ec05e23887f8a6b015
MohammadMahdi MoeinZade <moein1475963.mmz@gmail.com>
2021-07-18 18:49:24 +0430

    add profile api

--- backend/dashboard/urls.py
+++ backend/dashboard/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import MessageView
+from .views import MessageView, ProfileView
 
 urlpatterns = [
-    path('message', MessageView.as_view(), name="message")
+    path('message', MessageView.as_view(), name="message"),
+    path('profile', ProfileView.as_view(), name="profile")
 ]
