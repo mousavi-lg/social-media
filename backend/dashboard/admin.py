cd278a04de39d6d26e6436bcbb8b374889be4948
moosavi‏ <@gmail.com>
2021-07-18 18:57:17 +0430

    Add some features to message model

--- backend/dashboard/admin.py
+++ backend/dashboard/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Message
 
-# Register your models here.
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = (
+        'sender',
+        'message'
+    )
+    list_filter = (
+        'sender',
+    )
+    search_fields = [
+        'message'
+    ]
+    readonly_fields = (
+        'sender',
+        'receiver',
+        'message'
+    )
+    actions_on_bottom = False
+    actions_on_top = True
+admin.site.register(Message, MessageAdmin)
+
--- backend/dashboard/models.py
+++ backend/dashboard/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class UserData(models.Model):
-    user = models.ForeignKey(User, on_delete= models.DO_NOTHING)
 
 class Message(models.Model):
     class Meta:
